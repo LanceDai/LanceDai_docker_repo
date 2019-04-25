9c684bb29c06f51583d32b7e926b215e4ba9657f
15167501778 <603842325@qq.com>
2019-04-25 18:36:07 +0800

    test ssh

--- pull_kubernetes_images_from_aliyun.sh
+++ pull_kubernetes_images_from_aliyun.sh
@@ -5,29 +5,28 @@ set -e
 # Check version in https://kubernetes.io/docs/reference/setup-tools/kubeadm/kubeadm-init/
 # Search "Running kubeadm without an internet connection"
 # For running kubeadm without an internet connection you have to pre-pull the required master images for the version of choice:
-KUBE_VERSION=v1.10.3
+KUBE_VERSION=v1.14.1
 KUBE_PAUSE_VERSION=3.1
-ETCD_VERSION=3.1.12
-DNS_VERSION=1.14.8
+KUBE_ETCD_VERSION=3.3.10
+KUBE_COREDNS_VERSION=1.3.1
 
 GCR_URL=k8s.gcr.io
-ALIYUN_URL=registry.cn-shenzhen.aliyuncs.com/cookcodeblog
+ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/lance_dai/
 
-images=(kube-proxy-amd64:${KUBE_VERSION}
-kube-scheduler-amd64:${KUBE_VERSION}
-kube-controller-manager-amd64:${KUBE_VERSION}
-kube-apiserver-amd64:${KUBE_VERSION}
-pause-amd64:${KUBE_PAUSE_VERSION}
-etcd-amd64:${ETCD_VERSION}
-k8s-dns-sidecar-amd64:${DNS_VERSION}
-k8s-dns-kube-dns-amd64:${DNS_VERSION}
-k8s-dns-dnsmasq-nanny-amd64:${DNS_VERSION})
+images=(
+  kube-apiserver:${KUBE_VERSION}
+  kube-controller-manager:${KUBE_VERSION}
+  kube-scheduler:${KUBE_VERSION}
+  kube-proxy:${KUBE_VERSION}
+  pause:${KUBE_PAUSE_VERSION}
+  etcd:${KUBE_ETCD_VERSION}
+  coredns:${KUBE_COREDNS_VERSION})
 
 
 for imageName in ${images[@]} ; do
-  docker pull $ALIYUN_URL/$imageName
-  docker tag  $ALIYUN_URL/$imageName $GCR_URL/$imageName
-  docker rmi $ALIYUN_URL/$imageName
+  docker pull ${ALIYUN_URL}/${imageName}
+  docker tag  ${ALIYUN_URL}/${imageName} ${GCR_URL}/${imageName}
+  docker rmi ${ALIYUN_URL}/${imageName}
 done
 
 docker images
