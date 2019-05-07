75d551a5f7de34327d45cce573727dcce8dd8954
15167501778 <603842325@qq.com>
2019-05-07 19:04:53 +0800

    add k8s images and Dockerfile in 2019年 05月 07日 星期二 19:04:53 CST

--- pull_kubernetes_images_from_aliyun.sh
+++ pull_kubernetes_images_from_aliyun.sh
@@ -7,9 +7,9 @@ source "./source.sh"
 ALIYUN_URL=registry.cn-hangzhou.aliyuncs.com/lance_dai
 
 for image in ${images[@]}; do
-  docker pull ${ALIYUN_URL}/${image}:${versionMap[$image]}
-  docker tag ${ALIYUN_URL}/${image}:${versionMap[$image]} ${hubAddrMap}/${image}:${versionMap[$image]}
-  docker rmi ${ALIYUN_URL}/${image}:${versionMap[$image]}
+  docker pull ${ALIYUN_URL}/${image}:${versionMap[${image}]}
+  docker tag ${ALIYUN_URL}/${image}:${versionMap[${image}]} ${hubAddrMap[${image}]}/${image}:${versionMap[${image}]}
+  docker rmi ${ALIYUN_URL}/${image}:${versionMap[${image}]}
 done
 
 docker images
